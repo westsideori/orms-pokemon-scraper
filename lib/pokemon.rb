c35a9bf1cb5d40868cdde4de1743af7f231d051b
Ori Markowitz <ori.markowitz@gmail.com>
2020-11-25 14:43:59 -0500

    commit tree 9729360cdd81f5fbc819c5a09a55b575b92ae6ed

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,34 @@
 class Pokemon
+    attr_accessor :name, :type, :db
+    attr_reader :id
+
+    def initialize(id: nil, name:, type:, db:)
+        @id = id
+        @name = name
+        @type = type
+        @db = db
+    end
+
+    def self.save(name, type, db)
+        db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+    end
+
+    def self.find(id_num, db)
+        pokemon_info = db.execute("SELECT * FROM pokemon WHERE id=?", id_num).flatten
+        Pokemon.new(id: pokemon_info[0], name: pokemon_info[1], type: pokemon_info[2], db: db)
+    end
+
+
+
+    # def self.save
+    #     sql = <<-SQL
+    #     INSERT INTO pokemon (name, type, db)
+    #     VALUES (?, ?, ?)
+    #     SQL
+
+    #     DB[:conn].execute(sql, self.name, self.type, self.db)
+
+    #     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+    # end
+
 end
